3b584de2078099b69f42b552cfdad7b3bef15572
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-03-26 22:20:23 +0000

    Lots and lots of more things.

--- dbclasses.py
+++ dbclasses.py
@@ -31,28 +31,6 @@ class Post(SQLObject):
                                                                       self.pubDate.month,
                                                                       self.pubDate.day,
                                                                       self.postID)
-    def HaloScanComments(self):
-        return u'''
-          <a href="javascript:HaloScan('%s');" target="_self">
-          <script type="text/javascript">postCount('%s');</script></a>'''%(self.postID,self.postID)
-    def HaloScanTB(self):
-        return u'''
-        <a href="javascript:HaloScanTB('%s');" target="_self">
-        <script type="text/javascript">postCountTB('%s'); </script></a>'''%(self.postID,self.postID)
-    def Talkr(self):
-        return u'''
-        <a href='http://www.talkr.com/app/fetch.app?feed_id=15734&perma_link=%s'>Listen to this post</a>
-        '''%urllib.quote(self.myurl())
-    def FeedBurnerFlare(self):
-        return u'''
-        <script src="http://feeds.feedburner.com/~s/LateralOpinion?i=%s" type="text/javascript" charset="utf-8"></script>
-        '''%(self.myurl().replace('#','%23'))
-        
-    def TechnoratiTags(self):
-        tags=['<a href="http://technorati.com/tag/%s">%s</a>'%(x.nameID,x.nameID) for x in self.categories]
-        if tags:
-            return u"Topics: "+", ".join(tags)
-        return u""
         
     
 class Story(SQLObject):
@@ -68,11 +46,3 @@ class Story(SQLObject):
     categories=MultipleJoin('StoryCategories')
     def myurl(self):
         return "http://lateral.blogsite.org/stories/%s.html"%self.postID
-    def HaloScanComments(self):
-        return '''
-          <a href="javascript:HaloScan('STORY%s');" target="_self">
-          <script type="text/javascript">postCount('STORY%s');</script></a>'''%(self.postID,self.postID)
-    def HaloScanTB(self):
-        return '''
-        <a href="javascript:HaloScanTB('STORY%s');" target="_self">
-        <script type="text/javascript">postCountTB('STORY%s'); </script></a>'''%(self.postID,self.postID)
--- macros.py
+++ macros.py
@@ -1,10 +1,75 @@
-import datetime
 # -*- coding: utf-8 -*-
 
+import datetime
+from dbclasses import *
+head=[]
 
+#################################################################################
+### Things that should be in the config file
+#################################################################################
+
+blogName="Lateral Opinion"
 basepath=u"http://lateral.blogsite.org/"
 author=u"Roberto Alsina <ralsina@kde.org>"
+description=u"Roberto Alsina's blog"
+language="en"
+version="Bartleblog 0.0"
+feedburnerName="LateralOpinion"
+feedburnerStoriesName="LateralOpinionStories"
+haloscanUser="ralsina"
+talkrFeedID="15734"
 
+#################################################################################
+### <head> manipulation
+#################################################################################
+
+def addHead(headers):
+    global head
+    for h in headers:
+        if not h in head:
+            head.append(h)
+
+def insertHead():
+    return '\n'.join(head)
+
+def rstHead():
+    return [
+    '<link rel="stylesheet" type="text/css" href="%s">'%absoluteUrl('static/css/rst.css'),
+    '<link rel="stylesheet" type="text/css" href="%s">'%absoluteUrl('static/css/html.css'),
+    '<link rel="stylesheet" type="text/css" href="%s">'%absoluteUrl('static/css/bartleblog.css'),
+    '<link rel="stylesheet" type="text/css" href="%s">'%absoluteUrl('static/css/silvercity.css')
+    ]
+
+def roundedHead():
+    return [
+    '<script type="text/JavaScript" src="%s"></script>'%absoluteUrl('static/js/rounded_corners.js'),
+    '''
+     
+ <script type="text/JavaScript"> 
+
+   function round_all()
+   {
+     settings = {
+       tl: { radius: 20 },
+       tr: { radius: 20 },
+       bl: { radius: 20 },
+       br: { radius: 20 },
+       antiAlias: true,
+       autoPad: false
+     } 
+
+     var cornersObj = new curvyCorners(settings, "rounded");
+     cornersObj.applyCornersToAll();
+   }
+    YAHOO.util.Event.addListener(window, "load", round_all) 
+ </script>
+    '''
+    ]
+    
+#################################################################################
+### General Macros
+#################################################################################
+    
 def weblogPermaLink(post):
     date=post.pubDate
     return u"%sweblog/%s/%02d/%02d.html#%s"%(basepath,date.year,date.month,date.day,post.postID)
@@ -15,46 +80,201 @@ def getUrlForDay(date):
 def absoluteUrl(path):
     return basepath+path
 
-def copyright(year):
-    return u"&copy; %d-%d %s"%(year,datetime.date.today().year,author)
+def copyright():
+    earliest=Post.select(orderBy=Post.q.pubDate)[0].pubDate
+    return u"&copy; %d-%d %s"%(earliest.year,datetime.date.today().year,author)
+
+#################################################################################
+### Technorati Macros
+#################################################################################
+
     
+def technoratiSearch():
+    return '''
+      <script type="text/javascript" src="http://embed.technorati.com/embed/tpgux8rtif.js"></script>
+    '''
+
+def technoratiTags(post):
+    tags=['<a href="http://technorati.com/tag/%s">%s</a>'%(x.nameID,x.nameID) for x in post.categories]
+    if tags:
+        return u"Topics: "+", ".join(tags)
+    return u""
+
+    
+#################################################################################
+### Yahoo Grids (layout)
+#################################################################################
+    
+def yahooGridsHead():
+    return ['<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.2.0/build/reset-fonts-grids/reset-fonts-grids.css">']
+    
+#################################################################################
+### Yahoo Calendar widget
+#################################################################################
+        
+def yahooCalendarHead():
+    return [
+    '<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.2.0/build/calendar/assets/calendar.css">',
+    '<script type="text/javascript" src="http://yui.yahooapis.com/2.2.0/build/yahoo-dom-event/yahoo-dom-event.js"></script>',
+    '<script type="text/javascript" src="http://yui.yahooapis.com/2.2.0/build/calendar/calendar-min.js"></script>',
+    '<script type="text/JavaScript" src="%s"></script>'%absoluteUrl('static/js/sprintf.js')
+    ]
+        
+def yahooCalendarWidget(_date):
+    date=_date.replace(day=1)
+    if date.month==12:
+        nextmonth=date.replace(year=date.year+1,month=1)
+    else:
+        nextmonth=date.replace(month=date.month+1)
+    disabled = ['%d/%d/%d'%(date.month,x,date.year) for x in range(1,32)]
+    monthposts=Post.select(AND(Post.q.pubDate>=date,Post.q.pubDate<nextmonth))
+    selected=[]
+    for post in monthposts:
+        s='%d/%d/%d'%(post.pubDate.month,post.pubDate.day,post.pubDate.year)
+        selected.append(s)
+        if s in disabled:
+            disabled.remove(s)
+    disabled=','.join(disabled)
+    
+    return '''  <div id="cal1Container"></div>
+                <script>
+                    YAHOO.namespace("blog");
+                    function init() {
+                        YAHOO.blog.cal1 = 
+                            new YAHOO.widget.Calendar("cal1","cal1Container", {
+                                pagedate:"%d/%d"
+                                });
+                        YAHOO.blog.cal1.addRenderer("%s",YAHOO.blog.cal1.renderBodyCellRestricted);
+                        
+                        var selectedDate = function (type,args,obj) {
+                            var s1 = args[0];
+                            var s2 = s1[0];
+                            window.location=printf("%s/weblog/%%d/%%02d/%%02d.html",s2[0],s2[1],s2[2]);
+                        };                    
+                        YAHOO.blog.cal1.selectEvent.subscribe(selectedDate, YAHOO.blog.cal1, true);
+
+                        YAHOO.blog.cal1.render();
+                    }
+                    YAHOO.util.Event.addListener(window, "load", init);
+                </script>
+            '''%(date.month,date.year,disabled,basepath)
+
+#################################################################################
+### HaloScan Macros
+#################################################################################
+            
+def haloscanHead():
+    return ['<script type="text/javascript" src="http://www.haloscan.com/load/%s"> </script>'%haloscanUser]
+
+def haloscanComments(post):
+    if isinstance(post,Story):
+        id="STORY"+post.postID
+    else:
+        id=post.postID
+    return u'''
+        <a href="javascript:HaloScan('%s');" target="_self">
+        <script type="text/javascript">postCount('%s');</script></a>'''%(id,id)
+
+def haloscanTB(post):
+    if isinstance(post,Story):
+        id="STORY"+post.postID
+    else:
+        id=post.postID
+    return u'''
+        <a href="javascript:HaloScanTB('%s');" target="_self">
+        <script type="text/javascript">postCountTB('%s'); </script></a>'''%(id,id)
+
+def haloscanLatestComments():            
+    return u'''
+        <b>Latest Comments:</b><br>
+        <script type="text/javascript" src="http://www.haloscan.com/members/recent/%s/"> </script>
+        <style type="text/css">
+        #haloscan-recent { padding: 2px; margin: 2px; }
+        #haloscan-recent a { text-decoration: none; color: #333333;}
+        #haloscan-recent ul {  list-style: none; width: 100%%; overflow: hidden; margin: 0; padding: 0;}
+        #haloscan-recent li { text-align: justify; list-style: none; margin: 1px;}
+        #haloscan-recent li span.hsrname { color: #06c; font-weight: bold; }
+        #haloscan-recent li span.hsrmsg { font-weight: normal; }
+        </style>
+    '''%haloscanUser
+
+def haloscanChiclet():
+    return u'''
+    <a href="http://www.haloscan.com/">
+    <img width="88" height="31" src="http://www.haloscan.com/halolink.gif" border="0" alt="Weblog Commenting and Trackback by HaloScan.com" />
+    </a>'''
+    
+#################################################################################
+### StatCounter Macros
+#################################################################################
+    
+def statcounterChiclet():
+    return '''
+        <!-- Start of StatCounter Code -->
+        <script type="text/javascript" language="javascript">
+        var sc_project=1436219; 
+        var sc_invisible=0; 
+        var sc_partition=13; 
+        var sc_security="b91cd70a"; 
+        </script>
+        <script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script>
+        <noscript>
+          <a href="http://www.statcounter.com/" target="_blank"><img  src="http://c14.statcounter.com/counter.php?sc_project=1436219&amp;java=0&amp;security=b91cd70a&amp;invisible=0" alt="hitcounter" border="0"></a> 
+        </noscript>
+        <a href="http://my.statcounter.com/project/standard/stats.php?project_id=1436219&amp;guest=1">Stats</a>
+'''
+
+#################################################################################
+### FeedBurner Macros
+#################################################################################
+
+def feedburnerHead():
+    return ['<link rel="alternate" type="application/rss+xml" title="RSS" href="http://feeds.feedburner.com/%s">'%feedburnerName]
+    
+def feedburnerCounter():
+    return '''
+<a href="http://feeds.feedburner.com/LateralOpinion"><img src="http://feeds.feedburner.com/~fc/LateralOpinion?bg=99CCFF&amp;fg=444444&amp;anim=1" height="26" width="88" style="border:0" alt="" /></a>
+'''
+
+def feedburnerFlare(post):
+    if isinstance(post,Story):
+        n=feedburnerStoriesName
+    else:
+        n=feedburnerName
+    return u'''
+        <script src="http://feeds.feedburner.com/~s/%s?i=%s" type="text/javascript" charset="utf-8"></script>
+        '''%(n,post.myurl().replace('#','%23'))
+
+def feedburnerBanner():
+    return u'''
+    <td align=center>
+      <a href=http://feeds.feedburner.com/%s">
+        <img src="http://feeds.feedburner.com/%s.gif" alt="%s" style="border:0">
+      </a>
+    </td>    
+'''%(feedburnerName,feedburnerName,blogName)
+        
+#################################################################################
+### Talkr Macros
+#################################################################################
+
+def talkrLink(post):
+    return u'''
+        <a href='http://www.talkr.com/app/fetch.app?feed_id=%s&perma_link=%s'>Listen to this post</a>
+        '''%(talkrFeedID,urllib.quote(post.myurl()))
+        
+def talkrChiclet():
+    return u'''
+        <a href='http://www.talkr.com/app/cast_pods.app?feed_id=%s'>
+            <img src='http://images.talkr.com/images/xml-podcast.gif' alt='Link to Podcast (RSS feed) for this blog' border='0'>
+        </a>'''%talkrFeedID
+
+
 chunk={
-'first': u'''
-''',
-'second':u'''
-<span>
-All contents of this site written by me are free.<p> 
+'blurb':u'''
+All contents of this site written by me are free.
 Copy, modify, whatever., just put my name in it, and if you change the contents, 
 clearly say so in the same page. Please provide a link back to the original.
-</span><p>
-<b>Latest Comments:</b><br>
-<script type="text/javascript" src="http://www.haloscan.com/members/recent/ralsina/"> </script>
-<style type="text/css">
-#haloscan-recent { padding: 2px; margin: 2px; }
-#haloscan-recent a { text-decoration: none; color: #333333;}
-#haloscan-recent ul {  list-style: none; width: 100%; overflow: hidden; margin: 0; padding: 0;}
-#haloscan-recent li { text-align: justify; list-style: none; margin: 1px;}
-#haloscan-recent li span.hsrname { color: #06c; font-weight: bold; }
-#haloscan-recent li span.hsrmsg { font-weight: normal; }
-</style>
-<p style="align:right;">
-<!-- Start of StatCounter Code -->
-<script type="text/javascript" language="javascript">
-var sc_project=1436219; 
-var sc_invisible=0; 
-var sc_partition=13; 
-var sc_security="b91cd70a"; 
-</script>
 
-<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><a href="http://www.statcounter.com/" target="_blank"><img  src="http://c14.statcounter.com/counter.php?sc_project=1436219&amp;java=0&amp;security=b91cd70a&amp;invisible=0" alt="hitcounter" border="0"></a> </noscript>
-<a href="http://my.statcounter.com/project/standard/stats.php?project_id=1436219&amp;guest=1">Stats</a>
-<!-- End of StatCounter Code -->
-<br>
-
-<a href="http://feeds.feedburner.com/LateralOpinion"><img src="http://feeds.feedburner.com/~fc/LateralOpinion?bg=99CCFF&amp;fg=444444&amp;anim=1" height="26" width="88" style="border:0" alt="" /></a>
-<br>
-<a href='http://www.talkr.com/app/cast_pods.app?feed_id=15734'>
-    <img src='http://images.talkr.com/images/xml-podcast.gif' alt='Link to Podcast (RSS feed) for this blog' border='0'>
-</a>
 '''
 }
