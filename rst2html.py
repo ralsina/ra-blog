9eb4c26550a51436fcf66e88e1bc6f0727a87d7d
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-03-29 22:42:14 +0000

    Flickr directive and image style to go with it.

--- rst2html.py
+++ rst2html.py
@@ -1,12 +1,70 @@
 # -*- coding: utf-8 -*-
 import docutils.core
+import docutils.parsers.rst
+
+
+
+import flickrapi
+
+def flickr( name, arguments, options, content, lineno,
+        content_offset, block_text, state, state_machine ):
+    """
+    A directive that produces a nicely styled image from flickr in HTML.
+  
+    The syntax is this::
+  
+    .. image:: title
+    
+    Where title is the title you used in flickr.
+    """
+  
+    flickrAPIKey='a4b67aa4c75657b2db3ebc2f2f43c136'
+
+    fapi=flickrapi.FlickrAPI(flickrAPIKey,'ab116226d50899a7')
+    token = fapi.getToken(browser="firefox")
+    fname=arguments[0]
+    rsp = fapi.photos_search(api_key=flickrAPIKey,auth_token=token,user_id='me',text=fname)
+    
+    # Get the secret and ID of the photo
+    
+    if len(rsp.photos[0].photo)==0:
+        error = state_machine.reporter.error( "Can't find image called %s in Flickr"% fname, 
+                    docutils.nodes.literal_block(block_text, block_text), line=lineno )
+        return [error]
+
+    id=rsp.photos[0].photo[0]['id']
+    secret=rsp.photos[0].photo[0]['secret']
+    owner=rsp.photos[0].photo[0]['owner']
+    print rsp.photos[0].photo[0]['title']
+    
+    rsp = fapi.photos_getInfo(api_key=flickrAPIKey,auth_token=token,photo_id=id,secret=secret)
+    
+    farm=rsp.photo[0]['farm']
+    server=rsp.photo[0]['server']
+    
+    
+    url= 'http://www.flickr.com/photos/%s/%s'%(owner,id)
+    t_url='http://farm%s.static.flickr.com/%s/%s_%s_%s.jpg'%(
+            farm,server,id,secret,'m')
+    
+    html='<a href="%s"><img src="%s" class="flickr"></a>'%(url,t_url)
+  
+    raw = docutils.nodes.raw('',html, format = 'html')
+    return [raw]
+
+flickr.arguments = (1,0,0)
+flickr.options = {'name' : docutils.parsers.rst.directives.unchanged }
+flickr.content = 1
+
+# Simply importing this module will make the directive available.
+docutils.parsers.rst.directives.register_directive( 'flickr', flickr )
 
 # This code is taken from here: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/252170
 # Great tool, great recipe!
 
 import SilverCity
-import docutils.parsers.rst
 import StringIO
+
 
 def code_block( name, arguments, options, content, lineno,
              content_offset, block_text, state, state_machine ):
@@ -52,3 +110,4 @@ docutils.parsers.rst.directives.register_directive( 'code-block', code_block )
 
 def rst2html(rst):
     return docutils.core.publish_parts(rst,writer_name='html')['fragment']
+
