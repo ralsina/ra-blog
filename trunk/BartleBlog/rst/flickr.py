30fe51185d7136c7af58c802a3a15b7da645f1c6
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-04-03 00:10:36 +0000

    Import name bug

--- trunk/BartleBlog/rst/flickr.py
+++ trunk/BartleBlog/rst/flickr.py
@@ -3,7 +3,7 @@
 import docutils.core
 import docutils.parsers.rst
 
-import BartleBlog.util.flickrapi
+import BartleBlog.util.flickrapi as flickrapi
 
 def flickr( name, arguments, options, content, lineno,
         content_offset, block_text, state, state_machine ):
