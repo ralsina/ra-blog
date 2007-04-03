36ffc55f22387c84213a61384ff74017203b1f47
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-04-03 21:24:14 +0000

    A rst2html that tells you if there are errors.

--- BartleBlog/ui/Ui_main_config.py
+++ BartleBlog/ui/Ui_main_config.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/mnt/centos/home/ralsina/Desktop/proyectos/ra-blog/bartleblog/BartleBlog/ui/main_config.ui'
 #
-# Created: Tue Apr  3 12:20:44 2007
+# Created: Tue Apr  3 18:28:26 2007
 #      by: PyQt4 UI code generator 4.1.1
 #
 # WARNING! All changes made in this file will be lost!
