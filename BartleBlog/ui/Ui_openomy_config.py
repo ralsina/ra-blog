46916c88cf29fff634c4534f753992d3cd000290
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-04-04 00:03:42 +0000

    Tag editor loads tags

--- BartleBlog/ui/Ui_openomy_config.py
+++ BartleBlog/ui/Ui_openomy_config.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/mnt/centos/home/ralsina/Desktop/proyectos/ra-blog/bartleblog/BartleBlog/ui/openomy_config.ui'
 #
-# Created: Tue Apr  3 20:49:36 2007
+# Created: Tue Apr  3 21:07:59 2007
 #      by: PyQt4 UI code generator 4.1.1
 #
 # WARNING! All changes made in this file will be lost!
