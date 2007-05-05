2e90dfe0ca662ddaa490e3b820748ec49d97a998
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-05 21:47:06 +0000

    Help UI

---
+++ trunk/BartleBlog/ui/Ui_help.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/mnt/centos/home/ralsina/Desktop/proyectos/bartleblog/bartleblog/BartleBlog/ui/help.ui'
+#
+# Created: Sat May  5 18:51:22 2007
+#      by: PyQt4 UI code generator 4.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(QtCore.QSize(QtCore.QRect(0,0,800,600).size()).expandedTo(MainWindow.minimumSizeHint()))
+
+        self.centralWidget = QtGui.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+
+        self.hboxlayout = QtGui.QHBoxLayout(self.centralWidget)
+        self.hboxlayout.setMargin(9)
+        self.hboxlayout.setSpacing(6)
+        self.hboxlayout.setObjectName("hboxlayout")
+
+        self.splitter = QtGui.QSplitter(self.centralWidget)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+
+        self.widget = QtGui.QWidget(self.splitter)
+        self.widget.setObjectName("widget")
+
+        self.vboxlayout = QtGui.QVBoxLayout(self.widget)
+        self.vboxlayout.setMargin(0)
+        self.vboxlayout.setSpacing(6)
+        self.vboxlayout.setObjectName("vboxlayout")
+
+        self.label_2 = QtGui.QLabel(self.widget)
+        self.label_2.setObjectName("label_2")
+        self.vboxlayout.addWidget(self.label_2)
+
+        self.hboxlayout1 = QtGui.QHBoxLayout()
+        self.hboxlayout1.setMargin(0)
+        self.hboxlayout1.setSpacing(6)
+        self.hboxlayout1.setObjectName("hboxlayout1")
+
+        self.search = QtGui.QLineEdit(self.widget)
+        self.search.setObjectName("search")
+        self.hboxlayout1.addWidget(self.search)
+
+        self.searchButton = QtGui.QToolButton(self.widget)
+        self.searchButton.setIcon(QtGui.QIcon(":/icons/icons/find.png"))
+        self.searchButton.setObjectName("searchButton")
+        self.hboxlayout1.addWidget(self.searchButton)
+        self.vboxlayout.addLayout(self.hboxlayout1)
+
+        self.label = QtGui.QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.vboxlayout.addWidget(self.label)
+
+        self.contents = QtGui.QTreeWidget(self.widget)
+        self.contents.setObjectName("contents")
+        self.vboxlayout.addWidget(self.contents)
+
+        self.view = QtGui.QTextBrowser(self.splitter)
+        self.view.setObjectName("view")
+        self.hboxlayout.addWidget(self.splitter)
+        MainWindow.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("MainWindow", "Search:", None, QtGui.QApplication.UnicodeUTF8))
+        self.searchButton.setText(QtGui.QApplication.translate("MainWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Contents:", None, QtGui.QApplication.UnicodeUTF8))
+
+import icons_rc
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
