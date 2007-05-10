d2fd40f5798d49556fe7c10300ef17a1b345f005
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-10 23:06:12 +0000

    Menu Editor work in progress:
    Can:
    
    * Create entries
    
    * Move them up/down/in/out of the hierarchy
    
    * Delete them
    
    Can't:
    
    * Make them of any type other than "label"
    
    * Save them
    
    * Load them

--- BartleBlog/ui/Ui_menu_config.py
+++ BartleBlog/ui/Ui_menu_config.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/mnt/centos/home/ralsina/Desktop/proyectos/bartleblog/bartleblog/BartleBlog/ui/menu_config.ui'
 #
-# Created: Thu May 10 14:04:34 2007
+# Created: Thu May 10 20:04:42 2007
 #      by: PyQt4 UI code generator 4.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -24,44 +24,55 @@ class Ui_Form(object):
         self.vboxlayout.setSpacing(6)
         self.vboxlayout.setObjectName("vboxlayout")
 
-        self.list = QtGui.QListWidget(Form)
-        self.list.setObjectName("list")
-        self.vboxlayout.addWidget(self.list)
+        self.tree = QtGui.QTreeWidget(Form)
+        self.tree.setObjectName("tree")
+        self.vboxlayout.addWidget(self.tree)
 
         self.hboxlayout1 = QtGui.QHBoxLayout()
         self.hboxlayout1.setMargin(0)
         self.hboxlayout1.setSpacing(6)
         self.hboxlayout1.setObjectName("hboxlayout1")
 
+        self.new = QtGui.QToolButton(Form)
+        self.new.setIcon(QtGui.QIcon(":/icons/icons/filenew.svg"))
+        self.new.setObjectName("new")
+        self.hboxlayout1.addWidget(self.new)
+
+        self.delete = QtGui.QToolButton(Form)
+        self.delete.setEnabled(False)
+        self.delete.setIcon(QtGui.QIcon(":/icons/icons/delete.svg"))
+        self.delete.setObjectName("delete")
+        self.hboxlayout1.addWidget(self.delete)
+
+        self.line = QtGui.QFrame(Form)
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.hboxlayout1.addWidget(self.line)
+
         self.left = QtGui.QToolButton(Form)
+        self.left.setEnabled(False)
         self.left.setIcon(QtGui.QIcon(":/icons/icons/1leftarrow.svg"))
         self.left.setObjectName("left")
         self.hboxlayout1.addWidget(self.left)
 
         self.right = QtGui.QToolButton(Form)
+        self.right.setEnabled(False)
         self.right.setIcon(QtGui.QIcon(":/icons/icons/1rightarrow.svg"))
         self.right.setObjectName("right")
         self.hboxlayout1.addWidget(self.right)
 
         self.up = QtGui.QToolButton(Form)
+        self.up.setEnabled(False)
         self.up.setIcon(QtGui.QIcon(":/icons/icons/1uparrow.svg"))
         self.up.setObjectName("up")
         self.hboxlayout1.addWidget(self.up)
 
         self.down = QtGui.QToolButton(Form)
+        self.down.setEnabled(False)
         self.down.setIcon(QtGui.QIcon(":/icons/icons/1downarrow.svg"))
         self.down.setObjectName("down")
         self.hboxlayout1.addWidget(self.down)
-
-        self.delete = QtGui.QToolButton(Form)
-        self.delete.setIcon(QtGui.QIcon(":/icons/icons/delete.svg"))
-        self.delete.setObjectName("delete")
-        self.hboxlayout1.addWidget(self.delete)
-
-        self.new = QtGui.QToolButton(Form)
-        self.new.setIcon(QtGui.QIcon(":/icons/icons/filenew.svg"))
-        self.new.setObjectName("new")
-        self.hboxlayout1.addWidget(self.new)
         self.vboxlayout.addLayout(self.hboxlayout1)
         self.hboxlayout.addLayout(self.vboxlayout)
 
@@ -70,13 +81,13 @@ class Ui_Form(object):
         self.vboxlayout1.setSpacing(6)
         self.vboxlayout1.setObjectName("vboxlayout1")
 
-        self.label = QtGui.QLabel(Form)
+        self.QLabel = QtGui.QLabel(Form)
+        self.QLabel.setObjectName("QLabel")
+        self.vboxlayout1.addWidget(self.QLabel)
+
+        self.label = QtGui.QLineEdit(Form)
         self.label.setObjectName("label")
         self.vboxlayout1.addWidget(self.label)
-
-        self.label1 = QtGui.QLineEdit(Form)
-        self.label1.setObjectName("label1")
-        self.vboxlayout1.addWidget(self.label1)
 
         self.label_2 = QtGui.QLabel(Form)
         self.label_2.setObjectName("label_2")
@@ -90,10 +101,10 @@ class Ui_Form(object):
         self.data_type.setObjectName("data_type")
         self.vboxlayout1.addWidget(self.data_type)
 
-        self.lineEdit = QtGui.QLineEdit(Form)
-        self.lineEdit.setEnabled(False)
-        self.lineEdit.setObjectName("lineEdit")
-        self.vboxlayout1.addWidget(self.lineEdit)
+        self.extra_data = QtGui.QLineEdit(Form)
+        self.extra_data.setEnabled(False)
+        self.extra_data.setObjectName("extra_data")
+        self.vboxlayout1.addWidget(self.extra_data)
 
         spacerItem = QtGui.QSpacerItem(122,51,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
         self.vboxlayout1.addItem(spacerItem)
@@ -102,15 +113,15 @@ class Ui_Form(object):
         self.save.setObjectName("save")
         self.vboxlayout1.addWidget(self.save)
         self.hboxlayout.addLayout(self.vboxlayout1)
-        self.label.setBuddy(self.label)
+        self.QLabel.setBuddy(self.QLabel)
         self.label_2.setBuddy(self.type)
-        self.data_type.setBuddy(self.lineEdit)
+        self.data_type.setBuddy(self.extra_data)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
-        Form.setTabOrder(self.label,self.type)
-        Form.setTabOrder(self.type,self.lineEdit)
-        Form.setTabOrder(self.lineEdit,self.left)
+        Form.setTabOrder(self.QLabel,self.type)
+        Form.setTabOrder(self.type,self.extra_data)
+        Form.setTabOrder(self.extra_data,self.left)
         Form.setTabOrder(self.left,self.right)
         Form.setTabOrder(self.right,self.up)
         Form.setTabOrder(self.up,self.down)
@@ -120,14 +131,20 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.new.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.delete.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
         self.left.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
         self.right.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
         self.up.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
         self.down.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
-        self.delete.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
-        self.new.setText(QtGui.QApplication.translate("Form", "...", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Form", "Label:", None, QtGui.QApplication.UnicodeUTF8))
+        self.QLabel.setText(QtGui.QApplication.translate("Form", "Label:", None, QtGui.QApplication.UnicodeUTF8))
         self.label_2.setText(QtGui.QApplication.translate("Form", "Type:", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Archives", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Label", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Link", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Story", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Tag", None, QtGui.QApplication.UnicodeUTF8))
+        self.type.addItem(QtGui.QApplication.translate("Form", "Tag List", None, QtGui.QApplication.UnicodeUTF8))
         self.save.setText(QtGui.QApplication.translate("Form", "Save", None, QtGui.QApplication.UnicodeUTF8))
 
 import icons_rc
