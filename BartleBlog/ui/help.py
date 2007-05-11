4e5bf585734b937dc8e23fff37cbcfeddb7c9907
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-11 21:45:17 +0000

    * Help that even loads.

--- BartleBlog/ui/help.py
+++ BartleBlog/ui/help.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtGui, QtCore
+import codecs
 
 from BartleBlog.ui.Ui_help import Ui_MainWindow
 
@@ -11,5 +12,6 @@ class HelpWindow(QtGui.QMainWindow):
         # Set up the UI from designer
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
-                
+
+        self.ui.view.setHtml(codecs.open('resources/help.html', 'r', 'utf-8').read())
         
