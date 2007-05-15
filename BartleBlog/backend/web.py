d97f9cc48effd47bd3c6ce7f0678b359254c3cd5
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-15 20:42:47 +0000

    Refactored dynamic-web-page-thingie (from now on bartleweb)

---
+++ BartleBlog/backend/web.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from colubrid import RegexApplication, HttpResponse, execute
+from colubrid.server import StaticExports
+from BartleBlog.backend.blog import Blog
+import BartleBlog.backend.dbclasses as db
+import os, codecs
+
+class webBlog(Blog):
+    def __init__(self):
+        Blog.__init__(self)
+        self.basepath='http://localhost:8080/'
+        self.dest_dir=os.path.expanduser("~/.bartleblog/preview")
+        if not os.path.isdir(self.dest_dir):
+            os.mkdir(self.dest_dir)
+        
+
+class MyApplication(RegexApplication):
+    blog=webBlog()
+    urls = [
+        (r'^(.*?)$', 'page'), 
+        (r'^(.*?)/(.*?)$', 'page'), 
+        (r'^(.*?)/(.*?)/(.*?)$', 'page'), 
+        (r'^(.*?)/(.*?)/(.*?)/(.*?)$', 'page')
+    ]
+
+    def page(self, *args):
+        path=''.join(args)
+        page=db.pageByPath(path)
+        self.blog.renderPage(page)
+        fn=os.path.join(self.blog.dest_dir, path)
+        resp=HttpResponse(codecs.open(fn).read())
+        #os.unlink(fn)
+        return resp
+
+def run():    
+    app = MyApplication
+    app = StaticExports(app, {'/static': os.path.expanduser('~/.bartleblog/static')})
+    execute(app)
--- bartleweb.py
+++ bartleweb.py
@@ -1,37 +1,3 @@
-from colubrid import RegexApplication, HttpResponse, execute
-from colubrid.server import StaticExports
-from BartleBlog.backend.blog import Blog
-import BartleBlog.backend.dbclasses as db
-import os, codecs
+import BartleBlog.backend.web as web
 
-class webBlog(Blog):
-    def __init__(self):
-        Blog.__init__(self)
-        self.basepath='http://localhost:8080/'
-        self.dest_dir=os.path.expanduser("~/.bartleblog/preview")
-        if not os.path.isdir(self.dest_dir):
-            os.mkdir(self.dest_dir)
-        
-
-class MyApplication(RegexApplication):
-    blog=webBlog()
-    urls = [
-        (r'^(.*?)$', 'page'), 
-        (r'^(.*?)/(.*?)$', 'page'), 
-        (r'^(.*?)/(.*?)/(.*?)$', 'page'), 
-        (r'^(.*?)/(.*?)/(.*?)/(.*?)$', 'page')
-    ]
-
-    def page(self, *args):
-        path=''.join(args)
-        page=db.pageByPath(path)
-        self.blog.renderPage(page)
-        return HttpResponse(codecs.open(os.path.join(self.blog.dest_dir, path)).read())
-
-app = MyApplication
-app = StaticExports(app, {
-    '/static': os.path.expanduser('~/.bartleblog/static')
-})
-
-if __name__ == '__main__':
-    execute(app)
+web.run()
