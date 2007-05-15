148fa361ed038d62168601080596999edf451a0e
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-15 02:33:10 +0000

    Full mako templates port.

--- branches/mako/BartleBlog/backend/blog.py
+++ branches/mako/BartleBlog/backend/blog.py
@@ -3,6 +3,7 @@
 import os,sys,datetime,codecs
 
 from mako.template import Template
+from mako.lookup import TemplateLookup
 
 import dbclasses as db
 from macros import Macros
@@ -59,24 +60,11 @@ class Blog:
         f=codecs.open(fname,"r","utf-8")
         return f.read()
 
-    def renderBlogPage(self,title,curDate,itemtmpl,pagetmpl,dname,fname,postlist=None,story=None,body=None,bodytitle=None):
-        macros=self.macros
-        blog=self
-        progress=self.progress
-        # Render
-        if body or body=="":
-            pass
-        else:
-            body=renderTemplate(self.loadTemplate(itemtmpl))
-
-        if pagetmpl:
-            page=renderTemplate(self.loadTemplate(pagetmpl))
-        else:
-            page=body
-        #page=slimmer.html_slimmer(page)
-        # Save or return
-        if not fname:
-            return page
+    def renderMakoPage(self, template, dname, fname, **kwargs):
+        lookup = TemplateLookup(directories=['./templates', '.'], module_directory='/tmp/mako_modules')
+        template = lookup.get_template(template)
+        kwargs['macros']=self.macros
+        page=template.render_unicode(**kwargs)
         if not os.path.exists(dname):
             os.makedirs(dname)
         if os.path.exists(fname):
@@ -85,7 +73,85 @@ class Blog:
         f.write(page)
         print 'saving: ', os.path.join(dname,fname)
 
+    def renderCategoryIndex(self):
+        title='%s posts by topic'%self.blog_title
+        dname=os.path.join(self.dest_dir,'categories')
+        fname='index.html'
+        curDate=datetime.datetime.today()
+        self.renderMakoPage(
+                'categoryIndexSite.tmpl', 
+                dname,
+                fname,
+                catlist=db.Category.select(),
+                title=title,
+                curDate=curDate
+            )
+
+    def renderCategory(self,cat):
+        catname=cat.name.lower()
+        title='Posts in %s about %s'%(self.blog_title,cat.name)
+        dname=os.path.join(self.dest_dir,'categories')
+        fname=catname+'.html'
+        postlist=cat.posts
+        postlist.reverse()
+        stlist=cat.stories
+        stlist.reverse()
+        curDate=datetime.datetime.today()
+        self.renderRSS(title,curDate,dname,catname+'.xml',postlist)
+        self.renderMakoPage(
+                'categorySite.tmpl',
+                dname,
+                fname,
+                title=title,
+                curDate=curDate,
+                postlist=postlist,
+                storylist=stlist, 
+                cat=cat
+            )
+        
+    def renderBlogMonth(self,date):
+        start=date.replace(day=1,hour=0,minute=0,second=0)
+        end=start
+        if start.month==12:
+            end=end.replace(year=start.year+1,day=1,month=1)
+        else:
+            end=end.replace(month=start.month+1)
+
+        postlist=db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end),
+                                orderBy='-pubDate')
+        if postlist.count()==0:
+            return
+
+        # Variables used by the templates
+        title=self.blog_title
+        curDate=postlist[0].pubDate
+        dname=os.path.join(self.dest_dir,"weblog/%d/%02d"%(date.year,date.month))
+        fname="index.html"
+
+        self.renderMakoPage(
+                'blogSite.tmpl',
+                dname,
+                fname, 
+                title=title,
+                curDate=curDate, 
+                postlist=postlist 
+            )
+
+        # FIXME: this should not be necessary, check it out
+            
+#        for day in range(1,32):
+#            try:
+#                self.renderBlogDay(curDate.replace(day=day))
+#            except ValueError:
+#                #To avoid checking month length
+#                pass
+                
+        # Render tool hooks for months        
+        for hook in self.monthHooks:
+            apply(hook,[date])
+
     def renderRSS(self,title,curDate,dname,fname,postlist):
+        return
         macros=self.macros
         blog=self
         rss=renderTemplate(self.loadTemplate('feedRSS'))
@@ -96,41 +162,6 @@ class Blog:
         f=codecs.open(os.path.join(dname,fname),"w","utf-8")
         f.write(rss)
 
-    def renderCategory(self,cat):
-        catname=cat.name.lower()
-        title='Posts in %s about %s'%(self.blog_title,cat.name)
-        dname=os.path.join(self.dest_dir,'categories')
-        fname=catname+'.html'
-        postlist=cat.posts
-        postlist.reverse()
-        curDate=datetime.datetime.today()
-        self.renderRSS(title,curDate,dname,catname+'.xml',postlist)
-        self.renderBlogPage(
-                title,
-                curDate,
-                'blogBriefSite',
-                'pageSite',
-                dname,
-                fname,
-                postlist=postlist,
-                bodytitle=title,
-            )
-
-    def renderCategoryIndex(self):
-        title='%s posts by topic'%self.blog_title
-        dname=os.path.join(self.dest_dir,'categories')
-        fname='index.html'
-        curDate=datetime.datetime.today()
-        self.renderBlogPage(
-                title,
-                curDate,
-                'categorySite',
-                'pageSite',
-                dname,
-                fname,
-                postlist=db.Category.select(),
-            )
-
     def renderCategories(self):
         self.renderCategoryIndex()
         for cat in db.Category.select():
@@ -139,71 +170,61 @@ class Blog:
     def renderStoryIndex(self):
         dname=os.path.join(self.dest_dir,'stories')
         fname="index.html"
-        postlist=db.Story.select(orderBy=db.Story.q.pubDate)
+        storylist=db.Story.select(orderBy='pubDate')
         title="%s - Story Index"%self.blog_title
         curDate=datetime.datetime.today()
-        self.renderBlogPage(
-                title,
-                curDate,
-                'storyIndex',
-                'pageSite',
+        self.renderMakoPage(
+                'storyIndexSite.tmpl',
                 dname,
                 fname,
-                postlist=postlist,
-                bodytitle='%s posts by topic'%self.blog_title
+                title=title,
+                curDate=curDate,
+                storylist=storylist,
             )
-        self.renderRSS(title,curDate,dname,'rss.xml',postlist)
+        self.renderRSS(title,curDate,dname,'rss.xml',storylist)
 
     def renderStory(self,story):
         story_dir=os.path.join(self.dest_dir,'stories')
         title=story.title
         curDate=story.pubDate
         fname="%s.html"%(story.postID)
-        self.renderBlogPage(
-                title,
-                curDate,
-                'storySite',
-                'pageSite',
+        self.renderMakoPage(                
+                'storySite.tmpl',
                 story_dir,
                 fname,
+                title=title,
+                curDate=curDate,
                 story=story
                 )
         
     def renderStories(self):
-
         self.renderStoryIndex()
-
-        # Render stories
         for story in db.Story.select():
             self.renderStory(story)
             self.progress.step()
 
 
     def renderBlogIndex(self):
-        postlist=list(db.Post.select(orderBy=db.Post.q.pubDate).reversed()[:20])
+        postlist=db.Post.select(orderBy='-pubDate')[:20]
         curDate=postlist[0].pubDate
         title=self.blog_title
         dname=os.path.join(self.dest_dir,"weblog")
         self.renderRSS(title,curDate,dname,'rss.xml',postlist)
-        self.renderBlogPage(
-                title,
-                curDate,
-                'blogSite',
-                'pageSite',
+        self.renderMakoPage(
+                'blogSite.tmpl', 
                 dname,
                 'index.html',
+                title=title,
+                curDate=curDate,
                 postlist=postlist
             )
 
     def renderBlogDay(self,date):
         start=date.replace(hour=0,minute=0,second=0)
         end=date+datetime.timedelta(1)
-        postlist=db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end))
+        postlist=db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end), orderBy='-pubDate')
         if postlist.count()==0:
             return
-
-        postlist=list(postlist)
-        postlist.reverse()
 
         # Variables used by the templates
         title=self.blog_title
@@ -211,136 +232,48 @@ class Blog:
         dname=os.path.join(self.dest_dir,"weblog/%d/%02d"%(date.year,date.month))
         fname="%02d.html"%(date.day)
 
-        self.renderBlogPage(
-                title,
-                curDate,
-                'blogSite',
-                'pageSite',
+        self.renderMakoPage(
+                'blogSite.tmpl', 
                 dname,
                 fname,
+                title=title,
+                curDate=curDate,
                 postlist=postlist
             )
         # Render tool hooks for days
         for hook in self.dayHooks:
             apply(hook,[date])
 
-    def renderBlogMonth(self,date):
-        start=date.replace(day=1,hour=0,minute=0,second=0)
-        end=start
-        if start.month==12:
-            end=end.replace(year=start.year+1,day=1,month=1)
-        else:
-            end=end.replace(month=start.month+1)
 
-        postlist=db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end),
-                                orderBy=db.Post.q.pubDate)
-        if postlist.count()==0:
-            return
-
-        postlist=list(postlist)
-        postlist.reverse()
-
-        # Variables used by the templates
-        title=self.blog_title
-        curDate=postlist[0].pubDate
-        dname=os.path.join(self.dest_dir,"weblog/%d/%02d"%(date.year,date.month))
-        fname="index.html"
-
-        self.renderBlogPage(
-                title,
-                curDate,
-                'blogSite',
-                'pageSite',
-                dname,
-                fname,
-                postlist=postlist
-            )
-
-        # FIXME: this should not be necessary, check it out
-            
-        for day in range(1,32):
-            try:
-                self.renderBlogDay(curDate.replace(day=day))
-            except ValueError:
-                #To avoid checking month length
-                pass
-                
-        # Render tool hooks for months        
-        for hook in self.monthHooks:
-            apply(hook,[date])
-        
+    def rederBlogPage (*args, **kwargs):
+        return
 
     def renderBlogYear(self,year):
-        for month in range(1,13):
-            self.renderBlogMonth(datetime.datetime(year=year,month=month,day=1))
-
         # Yearly archive page
         start=datetime.datetime(year=year,day=1,month=1)
-        contents=""
-        for month in range(1,13):
-            start=start.replace(day=1,month=month,hour=0,minute=0,second=0)
+        postlist=[]
+        for month in range(1, 13):
             end=start
             if start.month==12:
                 end=end.replace(year=start.year+1,day=1,month=1)
             else:
                 end=end.replace(month=start.month+1)
-
-            postlist=db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end),
-                                orderBy=db.Post.q.pubDate)
-
-            pc=postlist.count()
-            if pc==0:
-                continue
-            item=self.renderBlogPage(
-                    'Posts for month %d/%d'%(month,year),
-                    start,
-                    'blogBriefSite',
-                    'monthArchiveSite',
-                    None,
-                    None,
-                    postlist=postlist
-                )
-            contents+=item
-
-        body=contents
-        dname=os.path.join(self.dest_dir,'weblog',str(year))
-        fname='index.html'
-        self.renderBlogPage(
-            '%s for year %d'%(self.blog_title,year),
-            datetime.datetime(year=year,day=1,month=1),
-            None,
-            'pageSite',
-            dname,
-            fname,
-            body=body
-            )
+            postlist.append(db.Post.select(db.AND(db.Post.q.pubDate>=start,db.Post.q.pubDate<end),
+                                           orderBy='pubDate'))
+            start=end
+            
+        self.renderMakoPage ('yearSite.tmpl', 
+                             os.path.join(self.dest_dir,'weblog', str(year)), 
+                             'index.html', 
+                             title='Posts for year %d'%year,
+                             curDate= datetime.datetime(year=year,day=1,month=1), 
+                             postlist=postlist, 
+                             year=year,
+                             macros=self.macros
+                             )
         # Render tool hooks for years
         for hook in self.yearHooks:
             apply(hook,[date])
-
-    def renderBlogArchive(self):
-        plist=db.Post.select(orderBy=db.Post.q.pubDate)
-        start=plist[0].pubDate.year
-        end=plist[-1].pubDate.year
-        
-        body=''.join( [ '''
-        <div class="yui-u postbox thinedge"><a href="%s">Posts for year %d</a>        
-        </div>
-        '''%(self.macros.absoluteUrl('weblog/%d/index.html'%y),y) for y in range(start,end+1) ])
-
-        dname=os.path.join(self.dest_dir,'weblog')
-        fname='archive.html'
-
-        self.renderBlogPage(
-            '%s Archives'%(self.blog_title),
-            datetime.datetime.today(),
-            None,
-            'pageSite',
-            dname,
-            fname,
-            body=body,
-            bodytitle=self.blog_title
-            )
 
     def renderPage(self,page):
         print "rendering: ",page.path
@@ -368,8 +301,6 @@ class Blog:
         elif path[0]=='weblog':
             if path[1]=='index.html':
                 self.renderBlogIndex()                
-            elif path[1]=='archive.html':
-                self.renderBlogArchive()                
             elif len(path)==3:
                 if path[2]=='index.html':
                     year=int(path[1])
@@ -408,11 +339,10 @@ class Blog:
         if self.progress:
             self.progress.setStages([
             ['Rendering Categories','Rendering index pages for each category.'],
-            ['Rendering Archives','Rendering the historical archive index.'],
             ['Rendering Stories','Rendering the static story pages'],
             ['Rendering Index','Rendering the main page.'],
             ['Rendering Blog','Rendering all archived posts.']])
-        plist=db.Post.select(orderBy=db.Post.q.pubDate)
+        plist=db.Post.select(orderBy='pubDate')
         oldest=plist[0].pubDate
         newest=plist[-1].pubDate
 
@@ -424,24 +354,29 @@ class Blog:
             self.progress.setSteps(c)
         self.renderCategories()
 
-        if self.progress: self.progress.gotoStage(1)
-        self.renderBlogArchive()
 
         if self.progress:
-            self.progress.gotoStage(2)
+            self.progress.gotoStage(1)
             self.progress.setSteps(db.Story.select().count())
         self.renderStories()
 
-        if self.progress: self.progress.gotoStage(3)
+        if self.progress: self.progress.gotoStage(2)
         self.renderBlogIndex()
 
         if self.progress:
-            self.progress.gotoStage(4)
+            self.progress.gotoStage(3)
             # Each post is rendered once in year/month/day pages
             self.progress.setSteps(plist.count()*3)
             self.progress.setPos(0)
         for year in range(oldest.year,newest.year+1):
             self.renderBlogYear(year)
+            for month in range(1,13):
+                self.renderBlogMonth(datetime.datetime(year=year,month=month,day=1))
+                for day in range(1,32):
+                    try:
+                        self.renderBlogDay(datetime.datetime(year=year,month=month,day=day))
+                    except ValueError:
+                        pass
         if self.progress:
             self.progress.close()
 
@@ -456,7 +391,6 @@ class Blog:
         else:
             plist=db.Post.select(db.Post.q.is_dirty>1)
             slist=db.Story.select(db.Story.q.is_dirty>1)
-
 
         if self.progress:
             self.progress.gotoStage(0)
