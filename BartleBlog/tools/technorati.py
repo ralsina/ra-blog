bfee0106aaf82e056976d211825f3997ea93fd96
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-17 21:32:38 +0000

    * This tool now requires configuration instead of having my name hardcoded
    * Added topTags gadget: what your blog is about.
    
    * Added linkCount gadget: find out who links to you (supposedly)

--- BartleBlog/tools/technorati.py
+++ BartleBlog/tools/technorati.py
@@ -15,9 +15,21 @@ class Technorati:
 
     def favorites(self):
         if self.user:
-            return '''<iframe src="http://widgets.technorati.com/faves/%s?t=posts" marginwidth="0" marginheight="0" height="400" width="100%%" frameborder="0" scrolling="auto" style="padding:0;border:none;" ></iframe>'''%self.user
+                return '''<iframe src="http://widgets.technorati.com/faves/%s?t=posts" marginwidth="0" marginheight="0" height="400" width="100%%" frameborder="0" scrolling="auto" style="padding:0;border:none;" ></iframe>'''%self.user
         else:
             return '''<b>You need to configure the technorati support</b>'''
+            
+    def topTags(self):
+        return '''\
+        <script src="http://widgets.technorati.com/t.js" type="text/javascript"> </script>
+        <a href="http://technorati.com/blogs/%s?sub=tr_tagcloud_t_ns" class="tr_tagcloud_t_js" style="color:#4261DF">View blog top tags</a>
+        '''%self.blog.basepath
+        
+    def linkCount(self, url):
+        return '''\
+        <script src="http://embed.technorati.com/linkcount" type="text/javascript"></script>
+        <a class="tr-linkcount" href="http://technorati.com/search/%s">View blog reactions</a>
+        '''%url
 
 def factory(blog):
     return Technorati(blog)
