07d77f61ed4ac55f4846a5a6dc2420fb49aee3a9
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-05-17 21:13:29 +0000

    It seems the technorati search gadget is not general enough.
    So, removed, since what was there is more properly used as a chunk.

--- trunk/BartleBlog/tools/technorati.py
+++ trunk/BartleBlog/tools/technorati.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import BartleBlog.backend.config as config
 
 class Technorati:
     def __init__(self,blog):
         self.blog=blog
-        self.user='ralsina'
-
-    def search(self):
-        return '''
-          <script type="text/javascript" src="http://embed.technorati.com/embed/tpgux8rtif.js"></script>
-        '''
+        self.user=config.getValue('technorati', 'username', None)
 
     def tags(self,post):
         tags=['<a href="http://technorati.com/tag/%s">%s</a>'%(x.name,x.name) for x in post.categories]
@@ -18,7 +14,10 @@ class Technorati:
         return u""
 
     def favorites(self):
-        return '''<iframe src="http://widgets.technorati.com/faves/%s?t=posts" marginwidth="0" marginheight="0" height="400" width="100%%" frameborder="0" scrolling="auto" style="padding:0;border:none;" ></iframe>'''%self.user
+        if self.user:
+            return '''<iframe src="http://widgets.technorati.com/faves/%s?t=posts" marginwidth="0" marginheight="0" height="400" width="100%%" frameborder="0" scrolling="auto" style="padding:0;border:none;" ></iframe>'''%self.user
+        else:
+            return '''<b>You need to configure the technorati support</b>'''
 
 def factory(blog):
     return Technorati(blog)
