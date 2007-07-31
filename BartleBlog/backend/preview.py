3f261a2f711c91f4e8e43dc2c07401c00de865c8
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2007-07-31 23:12:23 +0000

    Finally integrated bartleweb (previews) with bartleblog.
    It's very hackish, but it's working. Needs to have bartleweb.py in PATH.

--- BartleBlog/backend/preview.py
+++ BartleBlog/backend/preview.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from threading import Thread
-import os
+import os, subprocess
 
-class Preview(Thread):
-    is_running=False
-    def run(self):
-        print 'Starting Preview Thread'
-        self.cpid=os.spawnlp(os.P_NOWAIT, 'bartleweb.py', 'bartleweb.py')
-        print self.cpid
-        ret=os.waitpid(self.cpid, 0)
-        print 'Ending Preview Thread (%d)'%ret
-        self.is_running=False
+class Preview:
+    def __init__(self):
+        # FIXME this is pretty hackish
+        # See if it's running already
+        self.pn=os.path.expanduser('~/.bartleblog')+'/bartleweb.pid'        
+        if os.path.exists(self.pn):
+            self.pid=int(open(self.pn, 'r').read())
+            try:
+                os.kill(self.pid, 15)
+            except:
+                pass
+        print "Starting preview process"
+        self.pid=subprocess.Popen("bartleweb.py").pid
+        open(self.pn, 'w').write(str(self.pid))
     def stop(self):
-        if self.is_running:
-#            os.kill(self.pid)
-            pass
+        print "Killing preview process"
+        os.kill(self.pid,  15)
+        os.unlink (self.pn)
+        pass
         
