2f48cff2dea60f0ceeb1186ceb2b46adc7ada125
roberto.alsina <roberto.alsina@b97cbd7f-562c-0410-8830-0b0d5a7ae7ba>
2008-02-29 02:16:36 +0000

    Much nicer flickr support

--- trunk/BartleBlog/rst/flickr.py
+++ trunk/BartleBlog/rst/flickr.py
@@ -27,8 +27,15 @@ def htmlforphoto(photo,fname):
     id=photo['id']
     secret=photo['secret']
     owner=photo['owner']
-    print owner
-    sys.exit(2)
+    
+    rsp = fapi.people_getInfo(api_key=flickrAPIKey,auth_token=token,user_id=owner)
+    try:
+      realname=rsp.person[0].realname[0].elementText
+    except:
+      realname=rsp.person[0].username[0].elementText
+      
+    photosurl=rsp.person[0].photosurl[0].elementText
+    
     rsp = fapi.photos_getInfo(api_key=flickrAPIKey,auth_token=token,photo_id=id,secret=secret)
 
     farm=rsp.photo[0]['farm']
@@ -38,7 +45,7 @@ def htmlforphoto(photo,fname):
     t_url='http://farm%s.static.flickr.com/%s/%s_%s_%s.jpg'%(
             farm,server,id,secret,'m')
 
-    html='<div class="center"><a href="%s"><img src="%s" class="flickr" alt="%s"></a><div class="footerbox">%s by %s</div></div>'%(url,t_url,fname,fname,user)
+    html='<div class="center"><a href="%s"><img src="%s" class="flickr" alt="%s"></a><div class="footerbox">%s by <a href="%s">%s</a></div></div>'%(url,t_url,fname,fname,photosurl,realname)
     return html
 
 def flickr( name, arguments, options, content, lineno,
@@ -72,7 +79,6 @@ def flickr( name, arguments, options, content, lineno,
       
     else:
       user='me'
-      username='me'
     rsp = fapi.photos_search(api_key=flickrAPIKey,auth_token=token,user_id=user,text=fname)
 
     # Get the secret and ID of the photo
